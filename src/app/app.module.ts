448fff8429f376715fd90b3cba29d9b60df83444
johnny <johnnydevriese22@gmail.com>
2017-07-25 10:44:35 -0600

    created entities list and url service. Debugged compiling issue.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+// import { routing, appRoutingProviders} from './app.routing';
 import { CharactersService } from './shared/services/characters.service';
 import { CharactersListComponent } from './characters-list/characters-list.component';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
-import { EntitiesService } from './shared/services/entities.service';
+// import { EntitiesService } from './shared/services/entities.service';
+import { EntitiesListComponent } from './shared/components/entities-list/entities-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharactersListComponent,
-    NavigationComponent
+    NavigationComponent,
+    EntitiesListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [CharactersService, EntitiesService],
+  providers: [
+    CharactersService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
